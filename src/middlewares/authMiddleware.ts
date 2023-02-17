fcc571a3aaa2aabe79755c6a642548f0cdbb0b4d
Mark_Maistrenko <revanmayfall@gmail.com>
2023-02-17 21:10:30 +0400

    middlewares

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction,Response,Request } from "express"
+
+export const authMiddleware = async ( req: Request, res: Response, next : NextFunction)=> { 
+    if ( !req.headers.authorization){
+        res.send(401)
+        return
+    }
+    const token = req.headers.authorization?.split(" ")[1]
+
+    const userId = await jwtService.getUserIdByToken(token!)
+    if(userId){
+        req.user = await usersServer.findUserById(userId)
+        next()
+        return 
+    }
+    res.send(401)
+}
